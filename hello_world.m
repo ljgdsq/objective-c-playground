456ca61116e5ae5dd7636e615306c6bf27c7546d
zhengshulin <742706540@qq.com>
2024-05-14 16:05:44 +0800

    add person

--- hello_world.m
+++ hello_world.m
@@ -1,5 +1,20 @@
 #import <Foundation/Foundation.h>
 
+
+@interface Person : NSObject
+@property (readonly) NSString* firstName;
+@property NSString* lastName;
+-(void) speakSome;
+@end
+
+@implementation Person
+-(void) speakSome{
+    NSLog("hello Person");
+}
+
+@end
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         NSLog(@"Hello, world!");
